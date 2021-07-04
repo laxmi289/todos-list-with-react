afbb214d44afbcba46aa6b62d8468ce489bbac23
Laxmi Gowthami <gowthamivittal289@gmail.com>
2021-07-04 20:10:37 +0530

    Final Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem tenetur quae aspernatur veniam laudantium temporibus soluta provident impedit unde adipisci, exercitationem numquam! Quae nobis inventore quasi? Incidunt, rerum laudantium provident velit iure impedit animi.</p>
+        </div>
+    )
+}
